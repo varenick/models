92555b85af8958e4f048b22fa0894fdf946f645b
mburcev <mburcev@europa5.febras.net>
2017-07-28 23:32:23 +1000

    Added newsIR preparation notebook, added scripts for eval and decode

--- textsum/decode.sh
+++ textsum/decode.sh
@@ -1,10 +1,10 @@
 ../bazel-bin/textsum/seq2seq_attention \
-    --mode=train \
+    --mode=decode \
     --article_key=article \
     --abstract_key=abstract \
-    --data_path=data/train_food \
+    --data_path=data/decode_food \
     --vocab_path=data/vocab_food \
     --log_root=log_food \
-    --train_dir=log_food/train \
-    --num_gpus=1
+    --num_gpus=1 \
+    --decode_dir=log_food/decode 
 
--- textsum/eval.sh
+++ textsum/eval.sh
@@ -1,10 +1,10 @@
 ../bazel-bin/textsum/seq2seq_attention \
-    --mode=train \
+    --mode=eval \
     --article_key=article \
     --abstract_key=abstract \
-    --data_path=data/train_food \
+    --data_path=data/eval_food \
     --vocab_path=data/vocab_food \
     --log_root=log_food \
-    --train_dir=log_food/train \
-    --num_gpus=1
+    --eval_dir=log_food/eval \
+    --num_gpus=0
 
